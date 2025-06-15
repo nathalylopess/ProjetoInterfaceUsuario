5870121fc591362fee970409906e830ed2d66fb5
nathaly lopess <lopesnathaly1810@gmail.com>
2025-06-15 12:24:39 -0300

    adicionando os comentário que eu havia esquecido

--- nathaly_lopes_prova01/src/components/Contador.jsx
+++ nathaly_lopes_prova01/src/components/Contador.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 export default function Contador() {
     const [contador, setContador] = useState(0)
+    // Aqui eu armazeno a variável contador com valor zero e irei alterá-la através do setContador
 
 
     return (
         <div>
             <h1>Contador: {contador}</h1>
             <button style={{ backgroundColor: contador > 10 ? "green" : "black" }} onClick={() => setContador(contador + 1)} > Incrementar </button>
+            {/* Aqui eu faço um if e else para caso o valor de contador seja maior que dez, o backgroundColor será verde, senão, será preto */}
+            {/* Para o onClick a lógica é a seguinte: utilizo o setContador para fazer as alterações no contador e no botão de incrementar, eu adiciono 1, no botão decrementar, subtraio 1 */}
             <button style={{ backgroundColor: contador < 0 ? "red" : "black" }} onClick={() => setContador(contador - 1)} > Decrementar</button>
 
         </div>
--- nathaly_lopes_prova01/src/components/Listas.jsx
+++ nathaly_lopes_prova01/src/components/Listas.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { carros } from './dado_carros.js'
+// Importo carros do arquivo dados_carros.js
 
 export default function Listas() {
     const [listaCarros, setListaCarros] = useState(carros)
+    // Armazeno a lista "carros" (a que eu importei do js) na constante "listaCarros" para fazer as alterações com o filter
 
     function filtrar() {
+        // Crio a função "filtrar" para poder filtrar apenas os carros vermelhos
         const novaListaCarros = listaCarros.filter((carro) => carro.cor === "vermelho")
-        setListaCarros(novaListaCarros)
+        // crio uma nova contante que vai ser a que eu vou fazer a filtragem
+        // nela uso o filter e se o "carro" da "listaCarros" (que equivale a lista "carros" que foi importada)
+        // tiver a cor igual a "vermelho" ela permanecerá na "listaCarros"
+        setListaCarros(novaListaCarros) 
+        // acima eu apenas mudo a contante "listaCarros" para a lista que eu acabei de criar: "novaListaCarros"
     }
 
     return (
@@ -17,14 +24,19 @@ export default function Listas() {
                 {carros.map((carro) =>
                     <li>Modelo: {carro.modelo} | Ano: {carro.ano}</li>
                 )}
+                {/* utilizo o map para mostrar apenas o modelo e o ano de cada carro da lista "carros" */}
+                {/* ela mostra de cada carro da lista o mode e o ano  */}
             </ol>
             <h2>Carros vermelhos</h2>
             <button onClick={filtrar} >Filtrar</button>
+            {/* aqui eu faço a ação de filtrar a lista, ou seja, até o momento, nada tinha sido alterado
+            porque a função ainda não havia sido chamada */}
 
             <ol>
                 {listaCarros.map((carro) =>
                     <li>Modelo: {carro.modelo} | Ano: {carro.ano} | Cor {carro.cor} </li>
                 )}
+                {/* aqui ela mostra todos os carros da lista que eu ciltrei masi acima */}
             </ol>
 
         </div>
