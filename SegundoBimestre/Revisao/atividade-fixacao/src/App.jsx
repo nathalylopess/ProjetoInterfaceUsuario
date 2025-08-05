6ec26db23c520df2c9372de59a82182df21fc751
nathaly lopess <lopesnathaly1810@gmail.com>
2025-08-05 18:09:01 -0300

    Primeiros passos

---
+++ SegundoBimestre/Revisao/atividade-fixacao/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Desenvolvimento de interfaces com React.js</h1>
+    </>
+  )
+}
+
+export default App
